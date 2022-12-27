c610191b29226c6deee0478b9d6da321deac59fe
GOHU <dev.gohu@gmail.com>
2022-12-27 23:10:48 +0900

    Create uni_Python-Week13_project2.py
    2번 문제

---
+++ uni_Python-Week13_project2.py
@@ -0,0 +1,29 @@
+import shutil
+import os
+if not os.path.isdir('textFile/2'):
+    os.mkdir('textFile/2/')
+if not os.path.isdir('textFile/2/2-1'):
+    os.mkdir('textFile/2/2-1/')
+if not os.path.isdir('textFile/2/2-2'):
+    os.mkdir('textFile/2/2-2/')
+    
+################## 1 ###################
+
+inFile = open('textFile/2/2-1/text1.txt','r', encoding = 'utf-8')
+outFile = open('textFile/2/2-2/text1.txt', 'w', encoding = 'utf-8')
+
+print('원본 파일')
+
+for line in inFile.readlines():
+    print(line, end='')
+    outFile.writelines(line)
+    
+inFile.close()
+outFile.close()
+
+outFile = open('textFile/2/2-2/text1.txt', 'r', encoding = 'utf-8')
+print('\n\n복제된 파일')
+for line in outFile.readlines():
+    print(line, end='')
+    
+outFile.close()
